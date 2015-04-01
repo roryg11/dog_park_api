6b5ff12ae8789ad15e5fd3fe6ceb327b65b0aa10
roryg11 <Rory.C.Grant@gmail.com>
2015-04-01 13:48:25 -0700

    rendering geoJSON on geojson page

--- app/controllers/dog_parks_controller.rb
+++ app/controllers/dog_parks_controller.rb
@@ -3,4 +3,28 @@ class DogParksController < ApplicationController
     @dog_parks  = DogPark.all
     render json: @dog_parks
   end
+
+  def geojson
+    @parks = DogPark.all
+    @parks_geojson = Array.new
+
+    @parks.each do |park|
+      @parks_geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [park.longitude, park.latitude]
+        },
+        properties: {
+          name: park.name,
+          address: park.address,
+          description: park.description,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'medium',
+        }
+      }
+    end
+    render json: @parks_geojson
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'dog_parks#index'
+  get '/geojson' => 'dog_parks#geojson', as: 'geojson' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
